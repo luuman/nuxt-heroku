5ddf0dbb19f16ebd9f2e9298ee39a688ea29b80f
luuman <512769206@qq.com>
2018-09-04 14:49:51 +0800

    heroApi

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
         <a
           href="https://nuxtjs.org/"
           target="_blank"
-          class="button--green">Documentation</a>
+          class="button--green">Documetation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
